14ef5870bd1dfaea033bb9b28369c1235c2ff394
mattiabenelli <benellimattia0@gmail.com>
2022-12-01 17:44:05 +0100

    problema primo bonus

--- js/script.js
+++ js/script.js
@@ -39,17 +39,45 @@ const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 next.addEventListener('click', function(){
-    //verifico l'elemento attivo (itemActive)
-    //incremento il suo valore di 1
-    //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
-    //stessa cosa per i cerchi
+    if(itemActive <= 3){
+        //verifico l'elemento attivo (itemActive)
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        //incremento il suo valore di 1
+        itemActive++;
+        //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+        items[itemActive].classList.add('active');
+        //stessa cosa per i cerchi
+        circles[itemActive].classList.add('active');
+    }
+    else{
+        itemActive = 0;
+        items[1,2,3,4].classList.remove('active');
+        circles[1,2,3,4].classList.remove('active');
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+    }
 });
 
 prev.addEventListener('click', function(){
-    //verifico l'elemento attivo (itemActive)
-    //decremento il suo valore di 1
-    //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
-    //stessa cosa per i cerchi
-})
+    if(itemActive > 1){
+        //verifico l'elemento attivo (itemActive)
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        //decremento il suo valore di 1
+        itemActive--;
+        //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+        items[itemActive].classList.add('active');
+        //stessa cosa per i cerchi
+        circles[itemActive].classList.add('active');
+    }
+    else{
+        itemActive = 4;
+        items[0,1,2,3].classList.remove('active');
+        circles[0,1,2,3].classList.remove('active');
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+    }
+});
 
 
